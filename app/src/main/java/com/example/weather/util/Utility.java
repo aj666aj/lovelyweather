d1dc5b42c43f8564609286c68f7b558eeed37579
aj666 <936836692@qq.com>
2020-01-13 15:14:08 +0800

    添加解析县城信息的方法

--- app/src/main/java/com/example/weather/util/Utility.java
+++ app/src/main/java/com/example/weather/util/Utility.java
@@ -3,6 +3,7 @@ package com.example.weather.util;
 import android.text.TextUtils;
 
 import com.example.weather.db.City;
+import com.example.weather.db.County;
 import com.example.weather.db.Province;
 
 import org.json.JSONArray;
@@ -50,6 +51,26 @@ public class Utility {
 
         }
         return false;
+    }
+
+    public static boolean handleCountyResponse(String response, int cityId){
+        if (!TextUtils.isEmpty(response)){
+            try{
+                JSONArray allCounties = new JSONArray(response);
+                for (int i=0;i<allCounties.length();i++){
+                    JSONObject countyObject = allCounties.getJSONObject(i);
+                    County county = new County();
+                    county.setCountyName(countyObject.getString("name"));
+                    county.setCityId(cityId);
+                    county.setWeatherId(countyObject.getString("weather_id"));
+                    county.save();
+                }
+                return true;
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return  false;
     }
 
 }
