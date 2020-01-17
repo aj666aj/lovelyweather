b3aa050e1a776a3501f93c51bdc12aec38db82b9
aj666 <936836692@qq.com>
2020-01-17 20:06:12 +0800

    浮动按钮点击提示

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "点我目前没用", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
